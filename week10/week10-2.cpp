75c2197131362448302cc7af0ba000550c2770ba
RayUR4413 <14750181@me.mcu.edu.tw>
2026-04-29 11:46:07 +0800

    week10

---
+++ week10/week10-2.cpp
@@ -0,0 +1,22 @@
+///week10-2.cpp另外一種排序法
+///上週教Bubble sort 泡泡排序法 慢
+///今天教 Selection sort 選擇排序法 快一點
+#include <stdio.h>
+int main()
+{
+    int a[10] = {9,8,7, 6,5,4, 3,2,1, 0};
+    for(int k=0; k<10; k++)printf("%d ", a[k]);
+    printf("\n");
+
+    for(int i=0; i<10; i++){
+        for(int j=i+1; j<10; j++){
+            if(a[i] >a[j]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for(int k=0; k<10; k++)printf("%d ", a[k]);
+    printf("\n");
+}
